1a3b95a81d08f271baccb79186d77894900e4351
jasonm23 <jasonm23@gmail.com>
2022-12-13 08:14:15 +0700

    Removed bundled etd, Depends on MELPA/etd

--- dev/kurecolor-examples.el
+++ dev/kurecolor-examples.el
@@ -9,7 +9,7 @@
 ;;; Code:
 ;;
 ;; Local Variables:
-;; eval: (when (fboundp 'rainbow-mode) (rainbow-mode +1)) (font-lock-add-keywords nil '(("(examples\\|(group\\| => \\| ~> " (0 'font-lock-keyword-face))))
+;; eval: (when (fboundp 'rainbow-mode) (rainbow-mode +1)) (font-lock-add-keywords nil '(("(etd-examples\\|(etd-group\\| => \\| ~> " (0 'font-lock-keyword-face))))
 ;; no-byte-compile: t
 ;; End:
 
@@ -19,11 +19,8 @@
 (require 'dash)
 (require 'kurecolor)
 
-(defalias 'group 'def-example-group)
-(defalias 'examples 'defexamples)
-
-(group "Color/hue range grouping"
- (examples kurecolor-hue-group
+(etd-group "Color/hue range grouping"
+ (etd-examples kurecolor-hue-group
   (kurecolor-hue-group
     "#00FF00"
     '((group-a (340 . 120))
@@ -40,41 +37,41 @@
   (kurecolor-hue-group "#FFFF00") => 'yellow-green
   (kurecolor-hue-group "#FFFF00" kurecolor-simple-hue-groups) => 'green))
 
-(group "Utility functions"
- (examples kurecolor-clamp
+(etd-group "Utility functions"
+ (etd-examples kurecolor-clamp
    (kurecolor-clamp 1 -1.0 1.0)    => 1
    (kurecolor-clamp 2 -1.0 1.0)    => 1.0
    (kurecolor-clamp -2 -1.0 1.0)   => -1.0
    (kurecolor-clamp 0.1 -1.0 1.0)  => 0.1
    (kurecolor-clamp -0.1 -1.0 1.0) => -0.1)
 
- (examples kurecolor-to-8bit
+ (etd-examples kurecolor-to-8bit
    (kurecolor-to-8bit 0)      => 0.0
    (kurecolor-to-8bit 1)      => 255.0
    (kurecolor-to-8bit 0.3231) ~> 82.39
    (kurecolor-to-8bit 0.7)    => 178.5
    (kurecolor-to-8bit 0.25)   => 63.75)
 
- (examples kurecolor-interpolate
+ (etd-examples kurecolor-interpolate
    (kurecolor-interpolate "#FFFFFF" "#000000") => "#7F7F7F"
    (kurecolor-interpolate "#0077FF" "#111111") => "#084488"
    (kurecolor-interpolate "#FF7700" "#111111") => "#884408"
    (kurecolor-interpolate "#7F7F7F" "#7F7F7F") => "#7F7F7F"
    (kurecolor-interpolate "#000000" "#000000") => "#000000"))
 
-(group "CSS helpers"
- (examples kurecolor-hex-to-cssrgb
+(etd-group "CSS helpers"
+ (etd-examples kurecolor-hex-to-cssrgb
    (kurecolor-hex-to-cssrgb "#347291") => "rgb(52, 114, 145)"
    (kurecolor-hex-to-cssrgb "#000000") => "rgb(0, 0, 0)"
    (kurecolor-hex-to-cssrgb "#888888") => "rgb(136, 136, 136)"
    (kurecolor-hex-to-cssrgb "#FFFFFF") => "rgb(255, 255, 255)")
 
- (examples kurecolor-hex-to-cssrgba
+ (etd-examples kurecolor-hex-to-cssrgba
    (kurecolor-hex-to-cssrgba "#347291FF") => "rgba(52, 114, 145, 1.0000)"
    (kurecolor-hex-to-cssrgba "#34729100") => "rgba(52, 114, 145, 0.0000)"
    (kurecolor-hex-to-cssrgba "#34729180") => "rgba(52, 114, 145, 0.5020)")
 
- (examples kurecolor-cssrgb-to-hex
+ (etd-examples kurecolor-cssrgb-to-hex
    (kurecolor-cssrgb-to-hex "rgb(10%, 20%, 90%)")          => "#1933E5"
    (kurecolor-cssrgb-to-hex "rgba(100%, 100%, 100%, 1.0)") => "#FFFFFF"
    (kurecolor-cssrgb-to-hex "rgb(52, 114, 145)")           => "#347291"
@@ -83,90 +80,90 @@
    (kurecolor-cssrgb-to-hex "rgb(52, 114, 145)" t)         => "#347291FF"
    (kurecolor-cssrgb-to-hex "rgba(52, 114, 145, 0.5)" t)   => "#3472917F")
 
- (examples kurecolor-css-rgb-value-to-number
+ (etd-examples kurecolor-css-rgb-value-to-number
    (kurecolor-css-rgb-value-to-number "10") => 10
    (kurecolor-css-rgb-value-to-number "10%") => 25.5
    (kurecolor-css-rgb-value-to-number "100%") => 255.0))
 
-(group "Absolute set hue, saturation, brightness"
- (examples kurecolor-hex-set-hue
+(etd-group "Absolute set hue, saturation, brightness"
+ (etd-examples kurecolor-hex-set-hue
    (kurecolor-hex-set-hue "#FF7700" 0.5) => "#00FFFF"
    (kurecolor-hex-set-hue "#FF00FF" 0.5) => "#00FFFF"
    (kurecolor-hex-set-hue "#FFFF00" 0.5) => "#00FFFF")
 
- (examples kurecolor-hex-set-saturation
+ (etd-examples kurecolor-hex-set-saturation
    (kurecolor-hex-set-saturation "#FF7700" 0.5) => "#FFBB7F"
    (kurecolor-hex-set-saturation "#007700" 0.5) => "#3B773B"
    (kurecolor-hex-set-saturation "#FF0000" 0.5) => "#FF7F7F")
 
- (examples kurecolor-hex-set-brightness
+ (etd-examples kurecolor-hex-set-brightness
    (kurecolor-hex-set-brightness "#FF7700" 0.5) => "#7F3B00"
    (kurecolor-hex-set-brightness "#FF0000" 0.5) => "#7F0000"
    (kurecolor-hex-set-brightness "#FF0077" 0.5) => "#7F003B"))
 
-(group "Set hue, saturation, brightness from another hex color"
- (examples kurecolor-hex-set-brightness-from
+(etd-group "Set hue, saturation, brightness from another hex color"
+ (etd-examples kurecolor-hex-set-brightness-from
    (kurecolor-hex-set-brightness-from "#008800" "#000000") => "#888888"
    (kurecolor-hex-set-brightness-from "#00FF00" "#000000") => "#FFFFFF"
    (kurecolor-hex-set-brightness-from "#000000" "#00FF00") => "#000000")
 
- (examples kurecolor-hex-set-hue-from
+ (etd-examples kurecolor-hex-set-hue-from
    (kurecolor-hex-set-hue-from "#FF0000" "#00FF00") => "#FF0000"
    (kurecolor-hex-set-hue-from "#00FF00" "#FF0000") => "#00FF00"
    (kurecolor-hex-set-hue-from "#0088FF" "#880000") => "#004888")
 
- (examples kurecolor-hex-set-saturation-from
+ (etd-examples kurecolor-hex-set-saturation-from
    (kurecolor-hex-set-saturation-from "#000000" "#00FF00") => "#FFFFFF"
    (kurecolor-hex-set-saturation-from "#004400" "#00FF00") => "#00FF00"
    (kurecolor-hex-set-saturation-from "#FFBB7F" "#FF7700") => "#FFBA7F"))
 
-(group "RGB Hex to rgb"
- (examples kurecolor-hex-to-rgb
+(etd-group "RGB Hex to rgb"
+ (etd-examples kurecolor-hex-to-rgb
    (kurecolor-hex-to-rgb "#347291") ~> '(0.2039 0.4471 0.5686)
    (kurecolor-hex-to-rgb "#72FF91") ~> '(0.4471 1.0 0.5686)
    (kurecolor-hex-to-rgb "#720091") ~> '(0.4471 0.0 0.5686)))
 
-(group "RGBA Hex to rgba"
- (examples kurecolor-hex-to-rgba
+(etd-group "RGBA Hex to rgba"
+ (etd-examples kurecolor-hex-to-rgba
    (kurecolor-hex-to-rgba "#34729100") ~> '(0.2039 0.4471 0.5686 0.0)
    (kurecolor-hex-to-rgba "#FFFFFFFF") => '(1.0 1.0 1.0 1.0)
    (kurecolor-hex-to-rgba "#72009172") ~> '(0.44715 0.0 0.5686 0.4471)))
 
-(group "HSV/RGB/HEX (RGB) conversion"
-  (examples kurecolor-hex-to-hsv
+(etd-group "HSV/RGB/HEX (RGB) conversion"
+  (etd-examples kurecolor-hex-to-hsv
     (kurecolor-hex-to-hsv "#347291") ~> '(0.5556 0.6414 0.5686)
     (kurecolor-hex-to-hsv "#729134") ~> '(0.2222 0.6414 0.5686)
     (kurecolor-hex-to-hsv "#913472") ~> '(0.8889 0.6414 0.5686))
 
-  (examples kurecolor-hsv-to-hex
+  (etd-examples kurecolor-hsv-to-hex
     (kurecolor-hsv-to-hex 0.556 0.65 0.5687) => "#327191"
     (kurecolor-hsv-to-hex 1.0 0.7 1.0) => "#FF4C4C"
     (kurecolor-hsv-to-hex 0.5 0.5 0.6) => "#4C9999")
 
-  (examples kurecolor-hsv-to-rgb
+  (etd-examples kurecolor-hsv-to-rgb
     (kurecolor-hsv-to-rgb 0.5555 0.6413 0.5686) ~> '(0.2039 0.4472 0.5686))
 
-  (examples kurecolor-rgb-to-hex
+  (etd-examples kurecolor-rgb-to-hex
     (kurecolor-rgb-to-hex '(0.204 0.44706 0.5687)) => "#347291"))
 
-(group "Get hue, saturation, brightness"
- (examples kurecolor-hex-get-brightness
+(etd-group "Get hue, saturation, brightness"
+ (etd-examples kurecolor-hex-get-brightness
    (kurecolor-hex-get-brightness "#FFFFFF") => 1.0
    (kurecolor-hex-get-brightness "#808080") ~> 0.5020
    (kurecolor-hex-get-brightness "#000000") => 0.0)
 
- (examples kurecolor-hex-get-saturation
+ (etd-examples kurecolor-hex-get-saturation
     (kurecolor-hex-get-saturation "#00FF00") => 1.0
     (kurecolor-hex-get-saturation "#7FFF7F") ~> 0.5020
     (kurecolor-hex-get-saturation "#000000") => 0.0)
 
- (examples kurecolor-hex-get-hue
+ (etd-examples kurecolor-hex-get-hue
    (kurecolor-hex-get-hue "#FF0000") => 0.0
    (kurecolor-hex-get-hue "#00FF00") ~> 0.3333
    (kurecolor-hex-get-hue "#0000FF") ~> 0.6667))
 
-(group "Color adjust"
- (examples kurecolor-adjust-saturation
+(etd-group "Color adjust"
+ (etd-examples kurecolor-adjust-saturation
    (kurecolor-adjust-saturation "#006091" -0.1) => "#0E6491"
    (kurecolor-adjust-saturation "#006091" -0.2) => "#1C6991"
    (kurecolor-adjust-saturation "#006091" -0.5) => "#487891"
@@ -181,7 +178,7 @@
    (kurecolor-adjust-saturation "#166891"  1.0) => "#006091"
    (kurecolor-adjust-saturation "#FF0000" -2.0) => "#FFFFFF")
 
- (examples kurecolor-adjust-brightness
+ (etd-examples kurecolor-adjust-brightness
    (kurecolor-adjust-brightness "#FFFFFF" -0.1) => "#E5E5E5"
    (kurecolor-adjust-brightness "#FFFFFF" -0.2) => "#CCCCCC"
    (kurecolor-adjust-brightness "#FFFFFF" -0.5) => "#7F7F7F"
@@ -195,7 +192,7 @@
    (kurecolor-adjust-brightness "#AA6600"  0.1) => "#C37500"
    (kurecolor-adjust-brightness "#329847" -2.5) => "#000000")
 
- (examples kurecolor-adjust-hue
+ (etd-examples kurecolor-adjust-hue
    (kurecolor-adjust-hue "#FF0000" -0.1) => "#FF0098"
    (kurecolor-adjust-hue "#FF7700" -0.2) => "#FF00BB"
    (kurecolor-adjust-hue "#FFFF00" -0.5) => "#0000FF"
@@ -209,8 +206,8 @@
    (kurecolor-adjust-hue "#AA6600"  0.1) => "#87AA00"
    (kurecolor-adjust-hue "#329847"  0.5) => "#983183"))
 
-(group "XCode color literal helpers"
-  (examples kurecolor-hex-rgba-to-xcode-color-literal
+(etd-group "XCode color literal helpers"
+  (etd-examples kurecolor-hex-rgba-to-xcode-color-literal
     (kurecolor-hex-rgba-to-xcode-color-literal "#0E1B21FF")
     => "#colorLiteral(red: 0.0549019608, green: 0.1058823529, blue: 0.1294117647, alpha: 1.0000000000)"
     (kurecolor-hex-rgba-to-xcode-color-literal "#ECF3F600")
@@ -218,7 +215,7 @@
     (kurecolor-hex-rgba-to-xcode-color-literal "#ADC3CC80")
     => "#colorLiteral(red: 0.6784313725, green: 0.7647058824, blue: 0.8000000000, alpha: 0.5019607843)")
 
-  (examples kurecolor-xcode-literal-to-hex-rgba
+  (etd-examples kurecolor-xcode-literal-to-hex-rgba
     (kurecolor-xcode-color-literal-to-hex-rgba
      "#colorLiteral(red: 0.0864074271, green: 0.1963072013, blue: 0.2599330357, alpha: 1)")
     => "#163242FF"
@@ -232,7 +229,7 @@
      "#colorLiteral(red: 0.6817694399, green: 0.7659880177, blue: 0.802081694, alpha: 1)")
     => "#ADC3CCFF")
 
-  (examples kurecolor-xcode-literal-to-hex-rgb
+  (etd-examples kurecolor-xcode-literal-to-hex-rgb
     (kurecolor-xcode-color-literal-to-hex-rgb
      "#colorLiteral(red: 0.05882352941, green: 0.1098039216, blue: 0.0, alpha: 1)")
     => "#0E1C00"
